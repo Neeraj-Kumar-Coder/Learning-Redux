13e85b6ab4ef8f10940e4bd2863c0085a1d33f0b
Neeraj Kumar <nk7075034@gmail.com>
2023-05-16 14:19:58 +0530

    Learned about how to combine multiple reducers into a single root reducer to manage multiple states in a single store

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const redux = require("redux");
 
 // Out action type
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICE_CREAM = "BUY_ICE_CREAM";
 
 // Action creator
 function buy_cake(quantity = 1) {
@@ -12,15 +13,28 @@ function buy_cake(quantity = 1) {
         type: BUY_CAKE,
         payload: quantity
     };
-}
+};
+
+function buy_ice_cream(quantity = 1) {
+    // An action is an object with a 'type' property that describes the action
+    // actions only describes what happened, but not describe how the applications state would change
+    return {
+        type: BUY_ICE_CREAM,
+        payload: quantity
+    };
+};
 
 // Our initial state
-const initialState = {
+const initialCakeState = {
     numberOfCakes: 10
 };
 
+const initialIceCreamState = {
+    numberOfIceCreams: 20
+};
+
 // Reducers (they define how the state of the store would actually change depending upon the action specified)
-const reducer = (currentState = initialState, action) => {
+const cakeReducer = (currentState = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE:
             return {
@@ -30,9 +44,28 @@ const reducer = (currentState = initialState, action) => {
         default:
             return currentState;
     }
-}
+};
 
-const store = redux.createStore(reducer);
+const iceCreamReducer = (currentState = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICE_CREAM:
+            return {
+                ...currentState,
+                numberOfIceCreams: currentState.numberOfIceCreams - action.payload
+            };
+        default:
+            return currentState;
+    }
+};
+
+// Combining multiple reducers
+const rootReducer = redux.combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+});
+
+// Redux store
+const store = redux.createStore(rootReducer);
 console.log("Initial State = ", store.getState());
 
 const unsubscribe = store.subscribe(() => {
@@ -42,5 +75,7 @@ const unsubscribe = store.subscribe(() => {
 store.dispatch(buy_cake(1));
 store.dispatch(buy_cake(2));
 store.dispatch(buy_cake(3));
+store.dispatch(buy_ice_cream(3));
+store.dispatch(buy_ice_cream(1));
 unsubscribe();
-console.log("Finally the state looks like this", store.getState())
+console.log("Finally the state looks like this", store.getState());
